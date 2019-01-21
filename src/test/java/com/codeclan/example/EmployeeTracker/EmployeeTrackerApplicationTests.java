f071a9ca8b428415ecb7902d7445f9882af795d7
Borna Maticic <bornamaticic@yahoo.com>
2019-01-21 15:50:44 +0000

    Employee lab MVP.

---
+++ src/test/java/com/codeclan/example/EmployeeTracker/EmployeeTrackerApplicationTests.java
@@ -0,0 +1,29 @@
+package com.codeclan.example.EmployeeTracker;
+
+import com.codeclan.example.EmployeeTracker.models.Employee;
+import com.codeclan.example.EmployeeTracker.repositories.EmployeeRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeTrackerApplicationTests {
+
+	@Autowired
+	EmployeeRepository employeeRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void canSaveEmployee(){
+		Employee chris = new Employee("Chris", 28, 1, "Chris@gmail.com");
+		employeeRepository.save(chris);
+	}
+
+}
+
